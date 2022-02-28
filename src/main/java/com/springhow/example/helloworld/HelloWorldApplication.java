742ec8c0117eb54883235e568a39ea8d47b7e704
dpanigrahy2020 <66458175+dpanigrahy2020@users.noreply.github.com>
2022-02-28 23:25:33 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World DEBI!";
+        return "Hello World DEBI!!";
     }
 
 }
