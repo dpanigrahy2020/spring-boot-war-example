f48e31dee05852a246d5dc2a5ca1a3decc755db9
dpanigrahy2020 <66458175+dpanigrahy2020@users.noreply.github.com>
2022-03-07 18:05:58 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World!";
+        return "Welcome to CICD using AWS Pipeline";
     }
 
 }
