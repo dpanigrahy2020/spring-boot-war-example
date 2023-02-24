25a5ef68205160d19627fbbc4b3aa1e5b8c7a039
Debiprasad Panigrahy <debiprasadp@VALUEMOMENTUM.BIZ>
2023-02-24 14:32:53 +0530

    changed from Hello world to meaningfull name

--- src/main/java/com/springhow/example/getemployeedata/getEmployeeData.java
+++ src/main/java/com/springhow/example/getemployeedata/getEmployeeData.java
@@ -1,4 +1,4 @@
-package com.springhow.example.helloworld;
+package com.springhow.example.getemployeedata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,20 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class HelloWorldApplication extends SpringBootServletInitializer {
+public class getEmployeeData extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HelloWorldApplication.class);
+        return application.sources(getEmployeeData.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class);
+        SpringApplication.run(getEmployeeData.class);
     }
 
-
     @RequestMapping("/")
-    String helloWorld() {
+    String getData() {
         return "{\r\n\"Employees\" : [\r\n{\r\n\"userId\":\"rirani\",\r\n\"jobTitleName\":\"Developer\",\r\n\"firstName\":\"Romin\",\r\n\"lastName\":\"Irani\",\r\n\"preferredFullName\":\"Romin Irani\",\r\n\"employeeCode\":\"E1\",\r\n\"region\":\"CA\",\r\n\"phoneNumber\":\"408-1234567\",\r\n\"emailAddress\":\"romin.k.irani@gmail.com\"\r\n},\r\n{\r\n\"userId\":\"nirani\",\r\n\"jobTitleName\":\"Developer\",\r\n\"firstName\":\"Neil\",\r\n\"lastName\":\"Irani\",\r\n\"preferredFullName\":\"Neil Irani\",\r\n\"employeeCode\":\"E2\",\r\n\"region\":\"CA\",\r\n\"phoneNumber\":\"408-1111111\",\r\n\"emailAddress\":\"neilrirani@gmail.com\"\r\n},\r\n{\r\n\"userId\":\"thanks\",\r\n\"jobTitleName\":\"Program Directory\",\r\n\"firstName\":\"Tom\",\r\n\"lastName\":\"Hanks\",\r\n\"preferredFullName\":\"Tom Hanks\",\r\n\"employeeCode\":\"E3\",\r\n\"region\":\"CA\",\r\n\"phoneNumber\":\"408-2222222\",\r\n\"emailAddress\":\"d_panigrahy@yahoo.com\"\r\n}\r\n]\r\n}";
     }
 
