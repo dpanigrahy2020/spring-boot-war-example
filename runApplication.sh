2bfcbcdd61b4810e7de8c4f27956f9513bf1c7be
dpanigrahy2020 <66458175+dpanigrahy2020@users.noreply.github.com>
2023-02-26 09:36:14 +0530

    Update runApplication.sh

--- runApplication.sh
+++ runApplication.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps -eaf| grep jar| grep -v grep| awk '{print $2}'| xargs sudo kill -9
-nohup java -jar /home/ec2-user/employee-data-0.0.1-SNAPSHOT.jar 1>/dev/null 2>/dev/null &
+nohup java -jar /home/ec2-user/employee-data*.jar 1>/dev/null 2>/dev/null &
